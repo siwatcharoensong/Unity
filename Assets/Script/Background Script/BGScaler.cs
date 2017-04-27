160fcbfed3ec92e14aa4e220e120e88743ced5e1
siwat <the.life_meanningfull@hotmail.com>
2017-04-27 23:06:58 +0700

    first commit 27/04

---
+++ Assets/Script/Background Script/BGScaler.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BGScaler : MonoBehaviour {
+
+	void Start () {
+		SpriteRenderer sr = GetComponent<SpriteRenderer> ();
+		Vector3 tempScale = transform.localScale;
+
+		float width = sr.sprite.bounds.size.x;
+
+		float worlHeight = Camera.main.orthographicSize;
+	}
+
+}
---
+++ Assets/Script/Player Script/Player.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+	public float speed = 8f;
+	public float maxVLC = 4f;
+
+	[SerializeField]
+	private Rigidbody2D myBody;
+	private Animator anim;
+
+	void Awake() {
+		myBody = GetComponent<Rigidbody2D> ();
+		anim = GetComponent<Animator> ();
+	}
+
+	void Start () {
+		
+	}
+	 
+	void FixedUpdate () {
+		PlayerMoveKeyboard ();
+	}
+
+	void PlayerMoveKeyboard() {
+		float moveX = 0f;
+		float vel = Mathf.Abs (myBody.velocity.x);
+
+		float h = Input.GetAxisRaw ("Horizontal"); 
+
+		if (h > 0) {
+
+			if (vel < maxVLC)
+				moveX = speed;
+
+			Vector3 temp = transform.localScale;
+			temp.x = 2f;
+			transform.localScale = temp;
+
+			anim.SetBool ("Walk", true);
+	
+		} else if (h < 0) {
+
+			if (vel < maxVLC)
+				moveX = -speed;
+
+			Vector3 temp = transform.localScale;
+			temp.x = -2f;
+			transform.localScale = temp;
+
+			anim.SetBool ("Walk", true);
+		} else {
+
+			anim.SetBool ("Walk", false);
+		}
+
+		myBody.AddForce (new Vector2(moveX, 0));
+	}
+}
